6f401ac8161241d7f4f97ce9d110fb2987c87d6a
Katherine Stehberg <kstehberg@gmail.com>
2024-08-11 20:39:45 -0400

    corrected word

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get("/cafes/:id", (req, res) => {
     const { id } = req.params;
     const cafe = cafes.find(c => c.id == id);
     if (cafe) res.status(200).send(cafe);
-    else res.status(404).send({ message: "No se encontró ningún cafe con ese id" });
+    else res.status(404).send({ message: "No se encontró ningún café con ese id" });
 });
 
 app.post("/cafes", (req, res) => {
